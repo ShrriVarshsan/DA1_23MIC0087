be8f7b7aceb1021a026b12939b13d3cb15f4bd9d
Shrri Varshsan <shrrivarshsandr@gmail.com>
2026-01-09 20:50:47 +0530

    Initial commit - Java program

---
+++ DA1.java
@@ -0,0 +1,7 @@
+public class DA1 {
+    public static void main(String[] args) {
+        System.out.println("Name: D R SHRRI VARSHSAN");
+        System.out.println("Register Number: 23MIC0087");
+        System.out.println("Jenkins GitHub Integration Successful");
+    }
+}
